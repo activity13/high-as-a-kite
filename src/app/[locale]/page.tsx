df20c99ecb41f17f28e573123c75fe30bbbb986f
isaacvwolf@hotmail.com <isaacvelez13@gmail.com>
2025-08-15 23:48:20 -0500

    errores corregidos

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,4 @@
 import { useTranslations } from 'next-intl';
-import { Link } from '@/i18n/navigation';
-import Image from 'next/image';
 import Hero from '../components/Hero';
 export default function Home() {
   const t = useTranslations('translation.translations');
