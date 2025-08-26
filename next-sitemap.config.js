dc69613e06888cbd8ad5e90cd1c5d7d327e4414d
isaacvwolf@hotmail.com <isaacvelez13@gmail.com>
2025-08-25 22:06:43 -0500

    - Agregado next-sitemap, encargado de generar el sitemap del sitio y el robots.txt.

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'http://localhost:3000',
+  generateRobotsTxt: true, // (optional)
+  sitemapSize: 7000, // Number of URLs per sitemap file
+};
